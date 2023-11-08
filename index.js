d446f78a54c8effd89fa864d5351d5aa189f1c17
anikdas400 <anikchandra400@gmail.com>
2023-11-08 15:35:55 +0600

    delete function complite

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -43,6 +43,13 @@ async function run() {
       const result = await foodCollection.insertOne(newFood)
       res.send(result)
       console.log(newFood)
+    })
+
+    app.delete('/food/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query ={_id: new ObjectId(id)}
+      const result = await foodCollection.deleteOne(query)
+      res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
